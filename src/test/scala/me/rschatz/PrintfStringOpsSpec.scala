9bbfba724cae9b5b41b2f92e1af1fbae8c3ecdf4
rschatz <rschatz@ea.com>
2016-12-14 15:59:00 +0100

    adding more tests

--- src/test/scala/me/rschatz/PrintfStringOpsSpec.scala
+++ src/test/scala/me/rschatz/PrintfStringOpsSpec.scala
@@ -7,7 +7,11 @@ class PrintfStringOpsSpec extends WordSpec with Matchers {
   "PrintfStringOps" should {
     "support %u" in {
       "Foo %u Bar".formatx(1) shouldBe "Foo 1 Bar"
+      "Foo%s %u Bar".formatx("s", 1) shouldBe "Foos 1 Bar"
+      "Foo%s %u%s%u Bar".formatx("s", 1, "t", 2) shouldBe "Foos 1t2 Bar"
+      "Foo%s %u%u".formatx("s", 1, 2) shouldBe "Foos 12"
       "Foo %u Bar".formatx(-1) shouldBe "Foo 18446744073709551615 Bar"
+      "Foo %u Bar%s".formatx(-1, "s", 1) shouldBe "Foo 18446744073709551615 Bars"
     }
   }
 }
