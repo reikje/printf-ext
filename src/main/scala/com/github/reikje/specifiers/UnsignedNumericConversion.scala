f95bc1410d80ffe4f424f24771a98baebde2a420
rschatz <rschatz@ea.com>
2016-12-20 11:36:01 +0100

    improved error handling

--- src/main/scala/com/github/reikje/specifiers/UnsignedNumericConversion.scala
+++ src/main/scala/com/github/reikje/specifiers/UnsignedNumericConversion.scala
@@ -2,6 +2,8 @@ package com.github.reikje.specifiers
 
 import java.math.BigInteger
 
+import scala.util.Try
+
 /**
   * Converts unsigned numerics.
   *
@@ -11,13 +13,14 @@ trait UnsignedNumericConversion extends SupportedStringConversion {
   protected val complement: BigInteger
 
   override def convert(value: Any): String = {
-    val x = new BigInteger(value.toString)
-    val out = if (x.compareTo(BigInteger.ZERO) < 0) {
-      x.add(complement)
-    } else {
-      x
-    }
-    asString(out)
+    Try(new BigInteger(value.toString)).map { x =>
+      val out = if (x.compareTo(BigInteger.ZERO) < 0) {
+        x.add(complement)
+      } else {
+        x
+      }
+      asString(out)
+    }.getOrElse(throw new IllegalArgumentException(s"Cannot format $value using $specifier"))
   }
 
   protected def asString(value: BigInteger): String = value.toString
